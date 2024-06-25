187dca274e8ea3940e7e88f8e511218e0c3412ea
Maria Eduarda Vasconcelos <eduuardavasconcelos@gmail.com>
2024-06-25 17:28:07 -0300

    adição botão e funcionalidade editar descrição

--- src/components/CustomCard/CustomCard.jsx
+++ src/components/CustomCard/CustomCard.jsx
@@ -1,5 +1,5 @@
 /* eslint-disable react/prop-types */
-import { EditIcon } from "@chakra-ui/icons";
+import { EditIcon, CheckIcon } from "@chakra-ui/icons";
 import {
   Box,
   Button,
@@ -9,11 +9,34 @@ import {
   CardHeader,
   Flex,
   Heading,
+  Input,
   Stack,
   Text,
 } from "@chakra-ui/react";
+import { useState, useEffect } from "react";
+import { useDisciplinas } from "../../hooks/useDisciplinas";
 
-const CustomCard = ({ item, itemIndex, disciplinas }) => {
+const CustomCard = ({ item, itemIndex }) => {
+  const { setDisciplinas } = useDisciplinas();
+  const [isEditing, setIsEditing] = useState(false);
+  const [descricao, setDescricao] = useState(item.descricao);
+
+  useEffect(() => {
+    // Atualiza a descrição do item no array de disciplinas quando a descrição local mudar
+    setDisciplinas((prevDisciplinas) =>
+      prevDisciplinas.map((disciplina, index) =>
+        index === itemIndex ? { ...disciplina, descricao } : disciplina
+      )
+    );
+  }, [descricao, itemIndex, setDisciplinas]);
+
+  const handleEditClick = () => {
+    setIsEditing(!isEditing);
+  };
+
+  const handleDescricaoChange = (e) => {
+    setDescricao(e.target.value);
+  };
 
   return (
     <Flex marginRight={"2"}>
@@ -27,9 +50,18 @@ const CustomCard = ({ item, itemIndex, disciplinas }) => {
               <Heading size="xs" textTransform="uppercase">
                 Descrição:
               </Heading>
-              <Text pt="2" fontSize="sm">
-                {item.descricao}
-              </Text>
+              {isEditing ? (
+                <Input
+                  value={descricao}
+                  onChange={handleDescricaoChange}
+                  pt="2"
+                  fontSize="sm"
+                />
+              ) : (
+                <Text pt="2" fontSize="sm">
+                  {item.descricao}
+                </Text>
+              )}
             </Box>
             <Box>
               <Heading size="xs" textTransform="uppercase">
@@ -47,9 +79,9 @@ const CustomCard = ({ item, itemIndex, disciplinas }) => {
               width: 200,
             }}
             colorScheme="teal"
+            onClick={handleEditClick}
           >
-            {" "}
-            <EditIcon />{" "}
+            {isEditing ? <CheckIcon /> : <EditIcon />}
           </Button>
         </CardFooter>
       </Card>
